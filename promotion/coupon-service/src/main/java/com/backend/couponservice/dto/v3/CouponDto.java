31937dac382ef869035f254792043d3727d2713d
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-29 13:40:27 +0900

    feat: Kafka 사용 위한 Config 파일 생성

---
+++ promotion/coupon-service/src/main/java/com/backend/couponservice/dto/v3/CouponDto.java
@@ -0,0 +1,17 @@
+package com.backend.couponservice.dto.v3;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class CouponDto {
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class IssueMessage {
+        private Long policyId;
+        private Long userId;
+    }
+}
