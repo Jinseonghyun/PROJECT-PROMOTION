18d241b593ff80989c1def6be7551d383123a628
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-31 17:32:25 +0900

    feat: DB기반 타임 세일 테스트 코드

---
+++ promotion/time-sale-service/src/main/java/com/backend/timesaleservice/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.backend.timesaleservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/ui/product")
+    public String productPage() {
+        return "product";
+    }
+
+    @GetMapping("/ui/time-sale")
+    public String timeSale() {
+        return "timesale";
+    }
+}
